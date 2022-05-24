6c9c72fd024c62ff19e85f245fe2c0a46049d8bb
Colomk <41271648+Colomk@users.noreply.github.com>
2022-05-23 21:58:43 -0300

    Studies

---
+++ Udemy/Introdução a POO(Classes)/aula 54 - Classes(POO).py
@@ -0,0 +1,5 @@
+"""Programação Orientada a Objetos está presente em todas as linguagens de
+
+programação mais modernas segue o paradigma de programação onde você tenta retratar coisas do mundo
+
+real como objetos dentro do seu programa"""
