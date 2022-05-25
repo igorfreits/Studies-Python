656f37aa00b0ade0fc254d1cb62a5387a3ac4f76
Colomk <41271648+Colomk@users.noreply.github.com>
2022-05-24 22:25:36 -0300

    Update #021 - Tocando um MP3.py

--- Yggdrasil/Mundo 1 -  Floresta/#021 - Tocando um MP3.py
+++ Yggdrasil/Mundo 1 -  Floresta/#021 - Tocando um MP3.py
@@ -1,6 +1,8 @@
 import pygame
-
+import eme
 pygame.init()
-pygame.mixer.music.load('enemy.mp3')
+pygame.mixer.music.load(
+    r'C:\Users\Igor\Desktop\Estudos\Programação em Python\Mundo-invertido\Yggdrasil\Mundo 1 -  Floresta\enemy.mp3')
 pygame.mixer.music.play()
+input()
 pygame.event.wait()
