c19e6474d6b53e32d062e45eb9661380e4cf918b
Colomk <41271648+Colomk@users.noreply.github.com>
2022-06-07 00:08:36 -0300

    studies

---
+++ Udemy/Introdução a POO(Classes)/aula 73 -  DocStrings - Documentação.py
@@ -0,0 +1,17 @@
+"""DocStrings - Documentação"""
+# Letra maiúscula e uma breve descrição sobre oque o modulo faz
+# Str com multilinhas
+# Nome do Doc na primeira linha
+# Todos os itens são separados por categoria
+
+import uma_linha
+import funções_1
+import varias_linhas
+import funções_2
+import classe
+
+help(uma_linha)
+help(varias_linhas)
+help(funções_1)
+help(funções_2.multiplies)  # Escolhe qual modulo da documentação sera mostrado
+help(classe)
---
+++ Udemy/Introdução a POO(Classes)/varias_linhas.py
@@ -0,0 +1,20 @@
+# Aula 73
+'''Quick and simple description #Descrição rápida e simples
+
+
+Vamos supor que sonhamos:
+ora, nesse caso, as coisas inventadas parecem um
+bocado mais importantes do que as coisas reais.
+Vamos supor então que esta fossa, este seu reino,
+seja o único mundo existente. Pois, para mim, o
+seu mundo não basta. E vale muito pouco. E o que
+estou dizendo é engraçado, se a gente pensar bem.
+Somos apenas uns bebezinhos brincando, se é que
+a senhora tem razão, dona. Mas quatro crianças
+brincando podem construir um mundo de
+brinquedo que dá de dez a zero no seu mundo
+real. Por isso é que prefiro o mundo de brinquedo.
+
+Estou do lado de Aslam, mesmo que não haja
+Aslam. Quero viver como um narniano, mesmo
+que Nárnia não exista.'''
