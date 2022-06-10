5de9d54a2ffa0d58da71a76b7be2a682b316e39d
Colomk <41271648+Colomk@users.noreply.github.com>
2022-06-09 22:25:08 -0300

    studies

---
+++ Udemy/Módulos Python/aula  78 - Datetime#1.py
@@ -0,0 +1,42 @@
+"""Datetime - Trabalhando com data e hora em Python"""
+from datetime import datetime, timedelta
+
+"https://docs.python.org/pt-br/3/library/datetime.html"
+
+
+"""DATETIME: contém data e hora civis, novamente sem considerar fuso horário.
+Se por exemplo o prazo de pagamento é "dia tal até 13:00",
+é responsabilidade do pagador saber se estamos no horário de verão ou não."""
+date = datetime(2022, 4, 21, 10, 53, 20)  # Ano-mes-dia/ hora:minutos:segundos
+print(date.strftime("%d/%m/%Y %H:%M:%S"))  # Formata a data pra padrão BR
+# strptime - Cria um objeto de data a partir de um string
+
+"""TIMESTAMP: é um número que determina um momento específico.
+Tipicamente é expresso como o "número de segundos desde 1/1/1970 00:00 em Londres",
+mas poderia ser qualquer outra base.
+A ideia do timestamp é que ele vale no mundo todo, ou seja,
+ele identifica o momento exato em que algo aconteceu.
+Um acontecimento com timestamp "0" aconteceu em 31/12/1969 às 21:00 no Brasil."""
+date1 = datetime.strptime('20/04/2022', "%d/%m/%Y")
+# Contagem de segundos dede 1/1/29170 ate a data enviada
+print(date1.timestamp())
+
+date2 = datetime.fromtimestamp(1650423600.0)  # Converte o timestamp para data
+print(date2)
+
+date3 = datetime.strptime('20/04/2015 20:00:00', '%d/%m/%Y %H:%M:%S')
+# Adiciona mais tempo na data
+date3 = date3 + timedelta(days=5, seconds=2*60*60)
+print(date3.strftime('%d/%m/%Y %H:%M:%S'))
+
+d1 = datetime.strptime('20/04/2015 20:00:00', '%d/%m/%Y %H:%M:%S')
+d2 = datetime.strptime('25/04/2015 22:33:00', '%d/%m/%Y %H:%M:%S')
+
+diference = d2 - d1  # Diferença de tempo
+print(d1>d2)
+print(diference)
+print(diference.seconds)  # Mostra em segundos(da Hora)
+print(diference.total_seconds())  # Segundos totais
+print(diference.days)  # Numero de dias
+
+print(d1.time())  # Exibi so a hora
---
+++ Udemy/Módulos Python/aula 79 -  Datetime #2.py
@@ -0,0 +1,46 @@
+"Dataclasses #2"
+
+from calendar import monthrange
+from datetime import datetime
+from locale import setlocale, LC_ALL
+from calendar import mdays
+
+setlocale(LC_ALL, 'pt_Br.utf-8')  # Seta o local baseado no computador
+
+
+date = datetime.now()
+current_month = int(date.strftime('%m'))
+
+formatting = date.strftime('%A, %d de %B de %Y')
+print(formatting)
+print(mdays)  # Mostra quantos dias tem os meses(Retorna um lista)
+
+# Mostra quantos dias tem mes da variável
+print(current_month, mdays[current_month])
+
+"""A função monthrange de calendar para pegar o número do dia na semana(entre 0-6) e último dia do mês(entre 28-31), exemplo:
+"""
+# Retorna uma tupla contendo o número do dia na semana (0-6)
+# e último dia de fevereiro de 2020
+print(monthrange(2020, 2))
+
+# Saída - (5, 29)
+# O 5 significa que é um sábado
+# O 29 significa que este é o último dia do mês
+"""O número do dia na semana vai de 0 a 6 (segunda é 0, domingo é 6).
+
+Caso queira retornar apenas um valor, você pode fazer o desempacotamento, assim:"""
+
+dia_semana, ultimo_dia = monthrange(2020, 2)
+print(ultimo_dia)  # Saída: 29 (último dia de fevereiro de 2020)
+"""Você também pode criar uma lista, assim como mdays, contendo todos os últimos dias de meses do ano atual:
+"""
+
+últimos_dias_de_meses_do_ano_atual = [
+    monthrange(datetime.now().year, mes)[1] for mes in range(1, 13)
+]
+print(últimos_dias_de_meses_do_ano_atual)
+# Saída: [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+# Observação: meu ano atual é 2020 neste momento
+"""Isso deve solucionar os problemas do ano bissexto.
+"""
