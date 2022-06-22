c0fb5d42c3317f06489b7f91416c18cf7a830c1e
Colomk <41271648+Colomk@users.noreply.github.com>
2022-06-21 23:30:44 -0300

    studies

--- Udemy/5 - Interface Grafica/aula 102 - Adicionando PyQT5 em código Python antigo.py
+++ Udemy/5 - Interface Grafica/aula 102 - Adicionando PyQT5 em código Python antigo.py
@@ -1,1 +1,36 @@
 """Adicionando PyQT5 em código Python antigo"""
+
+import sys
+from validacpf import validate
+from geradorcpf import generate
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from designer_cpf import *
+
+# Refactor do gerador e validador de cpf do mudulo 1 para usar dentro do PyQT5
+
+
+class NewCpf(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        super().setupUi(self)
+
+        self.btnGenerate.clicked.connect(self.generate_cpf)
+        self.btnValidate.clicked.connect(self.validate_cpf)
+
+    def generate_cpf(self):
+        self.labelReturn.setText(
+            str(generate())
+        )
+
+    def validate_cpf(self):
+        cpf = self.inputValidadeCpf.text()
+        self.labelReturn.setText(
+            str(validate(cpf))
+        )
+
+
+if __name__ == '__main__':
+    qt = QApplication(sys.argv)
+    cpf = NewCpf()
+    cpf.show()
+    qt.exec_()
