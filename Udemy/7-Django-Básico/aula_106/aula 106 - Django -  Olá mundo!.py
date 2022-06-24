2c05a528b97630aa5fa624aae6922b1561568185
Colomk <41271648+Colomk@users.noreply.github.com>
2022-06-23 21:08:00 -0300

    studies

---
+++ Udemy/7-Django-Básico/aula_106/aula 106 - Django -  Olá mundo!.py
@@ -0,0 +1,17 @@
+"""Django: Olá mundo!
+
+-Framework
+-Crianção de website (back-end)
+
+"""
+
+"""# django-admin startproject "Nome do projeto" .
+(inserir ponto para nao criar mais de uma pasta)
+
+-wsgi.py - posta o site na web
+
+-apps - sessões (EX: pagina inicial, produtos, sobre, blog)
+    Registrar o apps em INSTALLED_APPS(settings-principal)
+    Deve setar as sessões em urls(EX: ling que redimensiona pro blog)
+
+"""
