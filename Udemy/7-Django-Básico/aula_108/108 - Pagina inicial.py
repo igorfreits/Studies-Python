4c17a63cba93f6be51461b29c5f187f16c1e7037
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-06-27 23:58:15 -0300

    studies

---
+++ Udemy/7-Django-Básico/aula_108/108 - Pagina inicial.py
@@ -0,0 +1,12 @@
+"""Criando Página inicial no Django
+
+
+-Criar  um app home
+ou
+-Criar template(Verificar aula 107)
+    -Criar um arquivo views.py na pasta raiz
+        Configurar
+    -Criar pasta home e html dentro de pages configurando o html
+    -adicionar em urls
+
+"""
---
+++ Udemy/7-Django-Básico/aula_109/109 -  Arquivos estáticos.py
@@ -0,0 +1,10 @@
+"""Arquivos estáticos (CSS, JS, IMG...)
+
+
+-criar templates(107)
+criar dois urls,views,html dentro do app escolhido
+
+Criar pasta static
+Criar pasta vendor
+criar STATICFILES_DIR dentro de settings
+"""
