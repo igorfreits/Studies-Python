78f3a31f56145e4bfcaa635c00cb43cfa21c1228
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-06-29 22:27:58 -0300

    studies

---
+++ Udemy/0-Challenges-and-projects/Projeto-Agenda/contatos/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils import timezone
+
+"""
+CONTATOS
+id: INT (automático)
+nome: STR * (obrigatório)
+sobrenome: STR (opcional)
+telefone: STR * (obrigatório)
+email: STR (opcional)
+data_criação: DATETIME (automático)
+descrição: texto
+categoria: CATEGORIA (outro model)
+
+ CATEGORIA
+ id: INT
+ nome: STR * (obrigatório)
+"""
+# Create your models here.
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nome
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+
+    data_criação = models.DateField(default=timezone.now)
+    descrição = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    mostrar = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.nome
