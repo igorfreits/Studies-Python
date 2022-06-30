c850f3f01d44361d30ba27e80217ed507670aa75
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-06-29 22:27:58 -0300

    studies

---
+++ Udemy/0-Challenges-and-projects/Projeto-Agenda/contatos/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from . models import Categoria, Contato
+
+# Register your models here.
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone',
+                    'email', 'data_criação', 'categoria', 'mostrar')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    # list_filter = ('nome', 'sobrenome')
+    list_per_page = 10
+    search_fields = ('nome', 'sobrenome', 'telefone')
+    list_editable = ('telefone', 'mostrar')
+
+
+admin.site.register(Contato, ContatoAdmin)
+admin.site.register(Categoria)
