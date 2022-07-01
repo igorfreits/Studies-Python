1b38a552137bdb2617fa5bf5ed04c733153f9b1d
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-07-01 11:52:38 -0300

    studies

--- Udemy/0-Challenges-and-projects/Projeto-Agenda/accounts/models.py
+++ Udemy/0-Challenges-and-projects/Projeto-Agenda/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from contatos.models import Contato
+from django import forms
 # Create your models here.
+
+
+class FormContato(forms.ModelForm):
+    class Meta:
+        model = Contato
+        exclude = ()
