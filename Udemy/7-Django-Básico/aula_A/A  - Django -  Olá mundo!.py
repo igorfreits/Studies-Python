306f890d49d34d37c21522df9b5ed7f18f56254a
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-07-02 13:43:40 -0300

    studies

---
+++ Udemy/7-Django-Básico/aula_A/A  - Django -  Olá mundo!.py
@@ -0,0 +1,34 @@
+"""Django: Olá mundo!
+
+-https://docs.djangoproject.com/pt-br/4.0/
+-Framework
+-Crianção de website (back-end)
+-python manage.py collectstatic(Cria uma pasta com os arquivos statics)
+
+X - nome do projeto/app
+-Inicia projeto com todos os arquivos necessários
+    django-admin startproject "X" .(TERMINAL)
+    (inserir ponto para nao criar mais de uma pasta)
+
+-Criar um servidor local
+    python manage.py runserver(TERMINAL)
+        CTRL+C para cancelar o servidor local
+
+-Posta site na web
+    wsgi.py
+
+-Apps - sessões (EX: pagina inicial, produtos, sobre, blog)
+    python manage.py startapp "Nome do app"(TERMINAL)
+    Registrar em INSTALLED_APPS(settings.py-principal)
+        "X"."X"Config - blog.BlogConfig
+    Criar url referente ao app que foi criado(urls.py) - importar o include
+        path('X'/,include('X.urls)) - path('blog/', include('blog.urls'))
+        Criar urls.py dentro da pasta do app criado
+    Deve setar as sessões em urls(EX: ling que redimensiona pro blog)
+
+-Migrations - Base de dados
+
+
+Putty - Liga servidor
+FileZilla - Move arquivos do projeto pro servidor
+"""
