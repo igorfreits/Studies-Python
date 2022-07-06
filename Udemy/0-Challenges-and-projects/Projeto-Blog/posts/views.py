17a2dd4d9dfdc1201382b8594d4754daafa093ea
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-07-05 22:22:52 -0300

    studies

--- Udemy/0-Challenges-and-projects/Projeto-Blog/posts/views.py
+++ Udemy/0-Challenges-and-projects/Projeto-Blog/posts/views.py
@@ -12,7 +12,7 @@ from django.contrib import messages
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
-    paginate_by = 3
+    paginate_by = 10
     context_object_name = 'posts'
 
     # def get_queryset(self):
