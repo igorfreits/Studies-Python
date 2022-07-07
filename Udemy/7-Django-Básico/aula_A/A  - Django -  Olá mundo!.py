b13773245ddf762d2a3f92a01e6c613b095564b6
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-07-07 15:47:33 -0300

    studies

--- Udemy/7-Django-Básico/aula_A/A  - Django -  Olá mundo!.py
+++ Udemy/7-Django-Básico/aula_A/A  - Django -  Olá mundo!.py
@@ -1,34 +1,48 @@
 """Django: Olá mundo!
 
 -https://docs.djangoproject.com/pt-br/4.0/
+
 -Framework
 -Crianção de website (back-end)
--python manage.py collectstatic(Cria uma pasta com os arquivos statics)
+-Putty - Liga servidor
+-FileZilla - Move arquivos do projeto pro servidor
 
-X - nome do projeto/app
--Inicia projeto com todos os arquivos necessários
+
+
+-Iniciar um novo projeto com todos os arquivos necessários
     django-admin startproject "X" .(TERMINAL)
     (inserir ponto para nao criar mais de uma pasta)
 
 -Criar um servidor local
     python manage.py runserver(TERMINAL)
-        CTRL+C para cancelar o servidor local
+    CTRL+C para cancelar o servidor local
 
 -Posta site na web
     wsgi.py
 
--Apps - sessões (EX: pagina inicial, produtos, sobre, blog)
+-Criar um app - sessões (EX: pagina inicial, produtos, sobre, blog)
     python manage.py startapp "Nome do app"(TERMINAL)
     Registrar em INSTALLED_APPS(settings.py-principal)
-        "X"."X"Config - blog.BlogConfig
+        "X"."X"Config - blog.BlogConfig ou nome do app
+
     Criar url referente ao app que foi criado(urls.py) - importar o include
         path('X'/,include('X.urls)) - path('blog/', include('blog.urls'))
         Criar urls.py dentro da pasta do app criado
-    Deve setar as sessões em urls(EX: ling que redimensiona pro blog)
+    Deve setar as sessões em urls(EX: link que redimensiona pro blog)
 
 -Migrations - Base de dados
+    python manage.py makemigrations(TERMINAL)
+    python manage.py migrate(TERMINAL)
 
+-Criar um usuário administrador
+    python manage.py createsuperuser(TERMINAL)
 
-Putty - Liga servidor
-FileZilla - Move arquivos do projeto pro servidor
+-Copiar os arquivos estáticos para o servidor
+    python manage.py collectstatic(TERMINAL)
+    
+-Django SSL Server
+    pip install django-sslserver
+    INSTALLED_APPS = (...
+    "sslserver",...)
+
 """
