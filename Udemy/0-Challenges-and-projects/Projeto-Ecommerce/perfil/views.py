79194f24450611665b8933000e8cf6c5d7307539
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-07-11 18:01:54 -0300

    studies

--- Udemy/0-Challenges-and-projects/Projeto-Ecommerce/perfil/views.py
+++ Udemy/0-Challenges-and-projects/Projeto-Ecommerce/perfil/views.py
@@ -1,14 +1,91 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
 from django.views import View
 from django.http import HttpResponse
+from . import models
+from . import forms
+from django.contrib.auth.models import User
+import copy
 
 # Create your views here.
 
 
-class Criar(View):
+class BasePerfil(View):
+    template_name = 'perfil/criar.html'
+
+    def setup(self, *args, **kwargs):
+        super().setup(*args, **kwargs)
+        self.cart = copy.deepcopy(self.request.session.get('cart', {}))
+        self.perfil = None
+
+        if self.request.user.is_authenticated:
+            self.perfil = models.Perfil.objects.filter(
+                usuario=self.request.user).first()
+
+            self.contexto = {
+                'userform': forms.UserForm(data=self.request.POST or None,
+                                           usuario=self.request.user,
+                                           instance=self.request.user
+
+                                           ),
+                'perfilform': forms.PerfilForm(data=self.request.POST or None),
+            }
+        else:
+            self.contexto = {
+                'userform': forms.UserForm(data=self.request.POST or None
+
+                                           ),
+                'perfilform': forms.PerfilForm(data=self.request.POST or None),
+
+            }
+        self.userform = self.contexto['userform']
+        self.perfilform = self.contexto['perfilform']
+        self.renderizar = render(
+            self.request, self.template_name, self.contexto)
+
     def get(self, *args, **kwargs):
-        return HttpResponse("Criar")
+        return self.renderizar
+
+
+class Criar(BasePerfil):
+    def post(self, *args, **kwargs):
+        # if not self.userform.is_valid() or not self.perfilform.is_valid():
+        if not self.userform.is_valid():
+            return self.renderizar
+
+        username = self.userform.cleaned_data.get('username')
+        password = self.userform.cleaned_data.get('password')
+        email = self.userform.cleaned_data.get('email')
+        first_name = self.userform.cleaned_data.get('first_name')
+        last_name = self.userform.cleaned_data.get('last_name')
+
+        # Usuário logado
+        if self.request.user.is_authenticated:
+            usuario = User.objects.get(username=username)
+            usuario.username = get_object_or_404(
+                User, username=self.request.user.username)
+
+            if password:
+                usuario.set_password(password)
+
+            usuario.email = email
+            usuario.first_name = first_name
+            usuario.last_name = last_name
+            usuario.save()
+
+        # Usuário não logado(novo)
+        else:
+            usuario = self.userform.save(commit=False)
+            usuario.set_password(password)
+            usuario.save()
+
+            perfil = self.perfilform.save(commit=False)
+            perfil.usuario = usuario
+            perfil.save()
+        self.request.session['cart'] = self.cart
+        self.request.session.save()
+
+        return self.renderizar
 
 
 class Update(View):
@@ -18,6 +95,7 @@ class Update(View):
 
 class Login(View):
     def get(self, *args, **kwargs):
+
         return HttpResponse("Login")
 
 
