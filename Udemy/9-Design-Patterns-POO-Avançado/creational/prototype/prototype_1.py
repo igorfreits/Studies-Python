9c3a12f1e606864914b043fe91b80098eafca2cc
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-07-19 00:09:20 -0300

    design pattern

---
+++ Udemy/9-Design-Patterns-POO-Avançado/creational/prototype/prototype_1.py
@@ -0,0 +1,49 @@
+"""
+Especificar os tipos de objetos a serem criados
+usando uma instância-protótipo e criar novos objetos
+pela cópia desse protótipo
+"""
+from __future__ import annotations
+from typing import List
+from copy import deepcopy
+
+
+class StringReprMixin:
+    def __str__(self):
+        params = ', '.join(
+            [f'{k}={v}' for k, v in self.__dict__.items()])
+        return f'{self.__class__.__name__}({params})'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Person(StringReprMixin):
+    def __init__(self, firstname: str, lastname: str) -> None:
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address: List = []
+
+    def add_address(self, address: Address) -> None:
+        self.address.append(address)
+
+    def clone(self) -> Person:
+        return deepcopy(self)
+
+
+class Address(StringReprMixin):
+    def __init__(self, street: str, number: str) -> None:
+        self.street = street
+        self.number = number
+
+
+if __name__ == '__main__':
+    igor = Person('Igor', 'Freitas')
+    endereco_igor = Address('Av. Brasil', '250A')
+    igor.add_address(endereco_igor)
+
+    michele = igor.clone()
+    michele.firstname = 'Michele'
+
+    print(str(igor))
+    print(str(michele))
