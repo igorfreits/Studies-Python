f6b4ae8a546c590c3c2e794af765aa90ee19d3de
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-07-18 23:10:12 -0300

    data patterns

--- Udemy/9-Design-Patterns-POO-Avançado/creational/singleton/monostate-2.py
+++ Udemy/9-Design-Patterns-POO-Avançado/creational/singleton/monostate-2.py
@@ -3,18 +3,16 @@ Monostate (ou Borg) - É uma variação do Singleton proposto
 por Alex Martelli que tem a intenção de garantir que o
 estado do objeto seja igual para todas as instâncias.
 """
-from __future__ import annotations
 from typing import Dict
 
 
 class StringReprMixin:
-    def __str__(self) -> str:
+    def __str__(self):
         params = ', '.join(
-            [f'{k}={v}' for k, v in self.__dict__.items()]
-        )
+            [f'{k}={v}' for k, v in self.__dict__.items()])
         return f'{self.__class__.__name__}({params})'
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return self.__str__()
 
 
@@ -26,10 +24,10 @@ class MonoState(StringReprMixin):
         obj.__dict__ = cls._state
         return obj
 
-    def __init__(self, nome=None, sobrenome=None) -> None:
+    def __init__(self, nome=None, sobrenome=None):
+        self.__dict__ = self._state
         if nome is not None:
             self.nome = nome
-
         if sobrenome is not None:
             self.sobrenome = sobrenome
 
@@ -38,8 +36,9 @@ class A(MonoState):
     pass
 
 
-if __name__ == "__main__":
-    m1 = MonoState(nome='Luiz')
-    m2 = A(sobrenome='Miranda')
+if __name__ == '__main__':
+    m1 = MonoState('Igor')
+    m2 = A(sobrenome='Freitas')
+
     print(m1)
     print(m2)
