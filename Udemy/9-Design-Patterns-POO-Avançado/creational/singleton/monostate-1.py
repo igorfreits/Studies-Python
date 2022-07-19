0da2b94c3d2692c4b8827c27136a492d663711bc
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-07-18 23:10:12 -0300

    data patterns

--- Udemy/9-Design-Patterns-POO-Avançado/creational/singleton/monostate-1.py
+++ Udemy/9-Design-Patterns-POO-Avançado/creational/singleton/monostate-1.py
@@ -3,36 +3,52 @@ Monostate (ou Borg) - É uma variação do Singleton proposto
 por Alex Martelli que tem a intenção de garantir que o
 estado do objeto seja igual para todas as instâncias.
 """
-from __future__ import annotations
+
+
 from typing import Dict
 
 
 class StringReprMixin:
-    def __str__(self) -> str:
+    def __str__(self):
         params = ', '.join(
-            [f'{k}={v}' for k, v in self.__dict__.items()]
-        )
+            [f'{k}={v}' for k, v in self.__dict__.items()])
         return f'{self.__class__.__name__}({params})'
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return self.__str__()
 
 
 class MonoStateSimple(StringReprMixin):
-    _state: Dict = {}
+    _state: Dict = {
+        'x': 10,
+        'y': 20
+    }  # Estado inicial do objeto
 
-    def __init__(self, nome=None, sobrenome=None) -> None:
+    def __init__(self, nome=None, sobrenome=None):
+        self.x = 100000  # Não afeta o valor do estado inicial
         self.__dict__ = self._state
-
+        #  self.x = 100000 - Mudou o valor depois que foi convertido o estado interno calasse
         if nome is not None:
             self.nome = nome
-
         if sobrenome is not None:
             self.sobrenome = sobrenome
 
 
-if __name__ == "__main__":
-    m1 = MonoStateSimple(nome='Luiz')
-    m2 = MonoStateSimple(sobrenome='Miranda')
+# class A(StringReprMixin):
+#     def __init__(self, nome):
+#         self.x = 20
+#         self.y = 10
+#         self.nome = nome
+
+
+# a = A('Igor')
+# print(a.__dict__)
+# print(a)
+if __name__ == '__main__':
+    m1 = MonoStateSimple('Igor')
+    m2 = MonoStateSimple(sobrenome='Freitas')
+    m1.x = 100
+    # Podemos ver que o m2 não foi alterado porque o estado é igual para todas as instâncias
+
     print(m1)
     print(m2)
--- Udemy/9-Design-Patterns-POO-Avançado/creational/singleton/singleton-3.py
+++ Udemy/9-Design-Patterns-POO-Avançado/creational/singleton/singleton-3.py
@@ -1,23 +1,24 @@
 # class Meta(type):
 #     def __call__(cls, *args, **kwargs):
-#         print('CALL é executado')
+#         print('Call é executado antes de instanciar')
 #         return super().__call__(*args, **kwargs)
 
 
-# class Pessoa(metaclass=Meta):
+# class Pessoa(metaclass = Meta):
 #     def __new__(cls, *args, **kwargs):
-#         print('NEW é executado')
+#         print('New é chamado antes do __init__')
+
 #         return super().__new__(cls)
 
 #     def __init__(self, nome):
-#         print('INIT é executado')
-#         self.nome = nome
+#         print('Init é chamado depois do __new__')
+#         self.nome=nome
 
 #     def __call__(self, x, y):
-#         print('Call chamado', self.nome, x + y)
+#         print('call', self.nome, x+y)
 
 
-# p1 = Pessoa('Luiz')
+# p1=Pessoa('Igor')
 # print(p1.nome)
 from typing import Dict
 
@@ -32,20 +33,21 @@ class Singleton(type):
 
 
 class AppSettings(metaclass=Singleton):
-    def __init__(self) -> None:
-        self.tema = 'O tema escuro'
+    def __init__(self):
+        self.tema = 'Tema escuro'
         self.font = '18px'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     as1 = AppSettings()
-
-    as1.tema = 'Qualquer outra coisa'
+    as1.tema = 'Tema claro'
 
     as2 = AppSettings()
-    as3 = AppSettings()
+    as2.tema = 'Tema vermelho'
 
+    as3 = AppSettings()
+    
     print(as3.tema)
     print(as1 == as2)
-    print(as1 == as3)
+    print(as1 is as3)
     print(as2 == as3)
