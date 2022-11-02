f379209f911cf6a7510875fb64f40448d4f286c5
Igor Freitas <41271648+igorfreits@users.noreply.github.com>
2022-11-02 10:01:14 -0300

    day 14

---
+++ 100-days/day-14/project-higher-lower.py
@@ -0,0 +1,61 @@
+from art import logo, vs
+from game_data import data
+import random
+from replit import clear
+
+
+def format_data(account):
+    """Takes the account data and returns the printable format."""
+    account_name = account["name"]
+    account_description = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_description}, from {account_country}"
+
+
+def check_answer(guess, a_followers, b_followers):
+    """Takes the user's guess and follower counts and returns if they got it right."""
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+
+# Display art
+print(logo)
+score = 0
+game_should_continue = True
+account_b = random.choice(data)
+
+# Make the game repeatable
+while game_should_continue:
+    account_a = account_b
+    account_b = random.choice(data)
+
+    while account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+
+    # Ask user for a guess
+    guess = input("Qual tem mais seguidores? 'A' ou 'B': ").lower()
+
+    # Check if user is correct
+    # Get follower count of each account
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    # Clear the screen between rounds
+    clear()
+
+    # Give user feedback on their guess
+    # Score keeping
+    if is_correct:
+        score += 1
+        print(f'Você acertou! Pontuação atual: {score}.')
+    else:
+        game_should_continue = False
+        print(f'Você errou! Pontuação final: {score}.')
